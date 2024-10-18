90f1351bf548cf06dc2f358cf23798d12a48dbcc
Haotian Gan <haotiangan@gmail.com>
2024-10-18 01:11:25 -0400

    Chapter 4 longer lexemes

--- Chapter4/Java/Scanner.java
+++ Chapter4/Java/Scanner.java
@@ -2,16 +2,101 @@ package Java;
 
 import java.util.ArrayList;
 import java.util.List;
+import static Java.TokenType.*;
 
 public class Scanner {
     private final String source; 
+    private final List<Token> tokens = new ArrayList<Token>();
+    private int start = 0; // points to first character in lexeme
+    private int current = 0; // points at the character being considered
+    private int line = 1;
     public Scanner(String source) {
         this.source = source;
     }
 
-    List<Token> scanTokens() {
-        ArrayList<Token> tokens = new ArrayList<Token>();
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case ' ': break;
+            case '\r': break;
+            case '\t': break;
+            case '\n':
+                line++;
+                break;
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case '\"':
+                
+                break;
+                
+            default:
+                Lox.error(line, "Unexpected character.");
+        }
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
+        return true;
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
         return tokens;
     }
 
