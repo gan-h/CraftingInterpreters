c6b8f30f7a88e2de2c3b724576b9ffa34e4388d7
Haotian Gan <haotiangan@gmail.com>
2024-10-22 16:26:57 -0400

    Visitor pattern

--- Chapter4/Java/Lox.java
+++ Chapter4/Java/Lox.java
@@ -51,6 +51,14 @@ public class Lox {
         }
     }
     public static void main(String[] args) throws IOException {
+        Expr expression = new Expr.Binary(
+        new Expr.Unary(
+        new Token(TokenType.MINUS, "-", null, 1),
+        new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+        new Expr.Literal(45.67)));
+        System.out.println(new ASTPrinter().print(expression));
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(0);
